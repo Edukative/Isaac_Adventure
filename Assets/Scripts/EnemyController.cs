288307c9e10251d1d81ecc808a4765c82f79c5ef
Edukative <56083723+Edukative@users.noreply.github.com>
2020-01-13 18:32:29 +0100

    animations

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -8,6 +8,11 @@ public class EnemyController : MonoBehaviour
     Rigidbody2D rb2D;
     public bool isVertical; //if it's not, it will walk horizontally
 
+    //timer values 
+    float timer;
+    int direction = 1;
+    public float changeTime = 3.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,18 +22,40 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timer -= Time.deltaTime; // decreases the timer by the time of the project built
+
+        if (timer < 0)
+        {
+            direction = -direction; // turns positive to negative and viceversa
+            timer = changeTime; // resets the timer
+        }
+
         Vector2 position = rb2D.position;
         // get the curreent position of the enemy
 
         if (isVertical) // if the enemy walks vertically
         {
             // same as isVertical == true;
-            position.y = position.y + Time.deltaTime * speed;
+            position.y = position.y + Time.deltaTime * speed * direction;
         }
-        position.x = position.x + Time.deltaTime * speed;
-        // sum the position x with the speed and the time
+        else
+        {
+            position.x = position.x + Time.deltaTime * speed * direction;
+            // sum the position x with the speed and the time 
+        }
+
 
         rb2D.MovePosition(position);
         // apply the previous sum position to the enemy's rigidbody
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+
+        if (player != null)
+        {
+            player.ChangeHealth(-1); // damages the player
+        }
     }
 }
