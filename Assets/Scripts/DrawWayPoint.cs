5cbc35f5ffa3b71014a2fc2ac5d662eb4d90cd4a
Edukative <56083723+Edukative@users.noreply.github.com>
2020-02-03 18:30:02 +0100

    wayPoint

---
+++ Assets/Scripts/DrawWayPoint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawWayPoint : MonoBehaviour
+{
+    public Transform target; // the next waypoint that the enemy goes
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Draws a white line from this transform to the target
+        Gizmos.color = Color.white;
+        Gizmos.DrawLine(transform.position, target.position);
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, 0.05f);
+    }
+}
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -16,26 +16,59 @@ public class EnemyController : MonoBehaviour
     // animator values
     Animator anim;
 
+    // waypoint values
+    public Vector2[] localNodes;
+    // private Vector[] worldNodes;
+    int currentNode;
+    int nextNode;
+    Vector2 Velocity;
+
     // Start is called before the first frame update
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>(); // get the enemy's RigidBody
         anim = GetComponent<Animator>(); // get the enemy's Animator
+
+        // waypoint stuff
+        localNodes = new Vector2[transform.childCount];
+
+        for (int i = 0; i <= transform.childCount - 1; ++i)
+        {
+            // from 0, to the lenght of the list, while is not there, sum 1 to the counter
+
+            Transform child = transform.GetChild(i).transform;
+            // get the transform of the child in the loop
+            localNodes[i] = new Vector2(child.transform.position.x, child.transform.position.y);
+            // put in the index a new Vector that is the position of the child
+            Debug.Log("index" + i + "Transform" + localNodes[i]);
+        }
+
+        currentNode = 0;
+        nextNode = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer -= Time.deltaTime; // decreases the timer by the time of the project built
+        /*timer -= Time.deltaTime; // decreases the timer by the time of the project built
 
         if (timer < 0)
         {
             direction = -direction; // turns positive to negative and viceversa
             timer = changeTime; // resets the timer
-        }
+        }*/
 
-        Vector2 position = rb2D.position;
-        // get the curreent position of the enemy
+        //Vector2 position = rb2D.position;// get the curreent position of the enemy
+        Vector2 wayPointDirection = localNodes[nextNode] - rb2D.position;
+        float dist = speed * Time.deltaTime;
+
+        if (wayPointDirection.sqrmagnitude < dist* dist) // if the enemy arrived to the
+        {
+            dist = wayPointDirection.magnitude;
+            currentNode = nextNode; // it arrived to the waypoint so it turns to the next
+            nextNode += 1;
+            if (nextNode >= localNodes.Length) // it reached the end
+        }
 
         if (isVertical) // if the enemy walks vertically
         {
